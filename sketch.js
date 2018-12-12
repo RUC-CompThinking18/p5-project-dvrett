84626c7f69925ea3363b4c039c2bb3d6525510a5
Dakota <dakota.everett@rutgers.edu>
2018-12-12 13:43:03 -0500

    Initial commit

--- sketch.js
+++ sketch.js
@@ -1,7 +1,43 @@
+let fighters = [];
+let stage;
+
+function preload(){
+  stage = loadImage('stage.jpg');
+  fighters = [loadImage('link.jpg'), loadImage('mario.jpg'), loadImage('luigi.jpg')];
+
 function setup() {
-  // put setup code here
+  createCanvas(1024, 576);
+  background(0);
 }
 
+//fighters will move each time the mouse is pressed
+function mousePressed() {
+  let r = random(100, 100);
+  let f = Fighter
+}
+
+
 function draw() {
-  // put drawing code here
+  background(stage);
+  for (let i = 0; i < fighters.length; i++) {
+    fighters[i].move();
+    fighters[i].show();
+  }
+}
+//creating class for individual fighter objects to be able to move across canvas
+class Fighter {
+  constructor(x, y, r){
+      this.x = x;
+      this.y = y;
+      this.r = r;
+  }
+move() {
+  this.x = this.x + random(-0.5, 0.5);
+  this.y = this.y + random(-0.5, 0.5);
+}
+  //show function to get images to appear
+  show() {
+    image(stage, this.x, this.y, this.r, this.r);
+    fill(0)
+  }
 }
